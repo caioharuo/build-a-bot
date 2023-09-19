1e5c55d08be1e572111be8a3075c493a6376d3ce
Caio Haruo <caio.haruo@hotmail.com>
2023-09-19 17:04:42 -0300

    initial commit

---
+++ src/home/HomePage.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  name: 'HomePage',
+};
+</script>
+
+<template>
+  <div class="home">
+    <div>
+      <img class="robot" src="../assets/robot-home.png" aria-hidden="true" />
+    </div>
+    <div class="get-started">
+      <a href="">Get started</a> building your first robot!
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.home {
+  text-align: center;
+}
+.robot {
+  height: 300px;
+}
+</style>
