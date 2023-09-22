316687af0bc5592baec79ed5b9acfbaedf4180d8
Caio Haruo <caio.haruo@hotmail.com>
2023-09-22 16:28:50 -0300

    Prevent invalid id

--- src/router/index.js
+++ src/router/index.js
@@ -62,6 +62,10 @@ export default createRouter({
       name: 'Parts',
       component: PartInfo,
       props: true,
+      beforeEnter(to, from, next) {
+        const isValidId = Number.isInteger(Number(to.params.id));
+        next(isValidId);
+      },
     },
   ],
 });
