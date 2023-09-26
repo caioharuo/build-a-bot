4ae18ac45f786fae9cf054ab96e18d4739fd229d
Caio Haruo <caio.haruo@hotmail.com>
2023-09-26 13:07:48 -0300

    Split pin directive

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -5,7 +5,12 @@
     </router-link>
     <button @click="selectPreviousPart()" class="prev-selector"></button>
     <button @click="selectNextPart()" class="next-selector"></button>
-    <span class="sale" v-show="selectedPart.onSale">Sale!</span>
+    <span
+      v-pin="{ bottom: '10px', right: '5px' }"
+      class="sale"
+      v-show="selectedPart.onSale"
+      >Sale!</span
+    >
   </div>
 </template>
 
@@ -82,9 +87,6 @@ export default {
   border: 3px solid #aaa;
 }
 .sale {
-  position: absolute;
-  bottom: 5px;
-  right: 5px;
   color: white;
   background-color: red;
   padding: 3px;
--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import { createApp } from 'vue';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import pinDirective from './shared/pin-directive';
 
-createApp(App).use(router).use(store).mount('#app');
+createApp(App)
+  .use(router)
+  .use(store)
+  .directive('pin', pinDirective)
+  .mount('#app');
